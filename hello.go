fa1aa217c4d857e39b4746bc9526e2775f3ddf9d
yang <94279769+nonetht@users.noreply.github.com>
2025-11-13 13:32:20 +0800

    pass the test, refactor test function

--- hello.go
+++ hello.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // 1.
 //func main() {
 //	fmt.Println("Hello, world")
@@ -9,7 +11,10 @@ import "fmt"
 
 // 2. 将代码和外部世界（side-effect）分离，才是也给好的测试方法。
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// 使用Go 这种静态语言的时候，编译器非常重要！
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+// t *testing.TB 为什么会出现问题呢？
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // 告诉测试套件，这是一个辅助方法
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
