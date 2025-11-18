5429c9e17066616e98e71465cbb89786ff55db84
yang <94279769+nonetht@users.noreply.github.com>
2025-11-18 13:25:41 +0800

    finish sum and sumall

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,6 @@
 package main
 
+// Sum 计算切片 numbers 的元素的总和
 func Sum(numbers []int) int {
 	sum := 0
 	// 使用 range，那么遍历的就是 index，value
@@ -7,4 +8,16 @@ func Sum(numbers []int) int {
 		sum += numbers
 	}
 	return sum
+}
+
+// SumAll 计算numbers（多个切片）的总和，然后按照顺序存储到 sums 数组之中。
+func SumAll(numbers ...[]int) []int {
+	lengthOfNumbers := len(numbers)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbers {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -28,6 +31,18 @@ func TestSum(t *testing.T) {
 
 		checkSum(t, got, want, numbers)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// slice can't compare to nil
+	// slices standard package is available, 但是该函数希望其中元素是comparable，因此对于二维切片则是不可行的！
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v, want %v", got, want) // got -> []
+	}
 }
 
 func checkSum(t *testing.T, got int, want int, numbers []int) {
