9d5f8ad730ea68534b93dffae3b867a8108c1e6e
yang <94279769+nonetht@users.noreply.github.com>
2025-11-18 11:02:42 +0800

    added french

--- hello.go
+++ hello.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // 1.
 //func main() {
@@ -10,13 +14,18 @@ const englishHelloPrefix = "Hello, "
 //}
 
 // 2. 将代码和外部世界（side-effect）分离，才是也给好的测试方法。
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	} else if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,22 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
-
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		// 更多的需求，现在我们要支持第二个参数，就是指定问候语的语言。
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
-// t *testing.TB 为什么会出现问题呢？
+// t *testing.TB 为什么会出现问题呢？是一个*testing.T, *testing.B 都满足的接口
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper() // 告诉测试套件，这是一个辅助方法
 	if got != want {
